2850abe64ff85008870a2a48d744cd30ed405759
Gaige Kinsey <gaigekinsey@yahoo.com>
2019-02-04 10:29:51 -0700

    Update UserInteraction.java

--- src/main/java/edu/neumont/cox/dentistoffice/view/UserInteraction.java
+++ src/main/java/edu/neumont/cox/dentistoffice/view/UserInteraction.java
@@ -7,4 +7,30 @@ public class UserInteraction implements UserInteractionInterface{
 	public String adminPasswordChange() {
 		return ConsoleIO.promptForInput("Admin must change their password on first login.\nWhat do you want your new password to be?", false);
 	}
+
+	@Override
+	public void noPermission() {
+		ConsoleIO.displayMessage("This requires Administrative privileges.");
+	}
+
+	@Override
+	public boolean checkPassword(String password) {
+		return password.equals(ConsoleIO.promptForInput("Please re-enter your password.", false));
+	}
+
+	@Override
+	public void passwordMismatch() {
+		ConsoleIO.displayMessage("Password doesn't match, please re-enter.");
+	}
+
+	@Override
+	public void dentistOfficeGreeting() {
+		ConsoleIO.displayMessage("Welcome to our Dentist Office!");
+	}
+
+	@Override
+	public int dentistOfficeMenu() {
+		
+		return 0;
+	}
 }
