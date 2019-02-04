1948298d8064f879b628d974d20741ab0096478a
Gaige Kinsey <gaigekinsey@yahoo.com>
2019-02-04 09:36:02 -0700

    Update UserInteraction.java

--- src/main/java/edu/neumont/cox/dentistoffice/view/UserInteraction.java
+++ src/main/java/edu/neumont/cox/dentistoffice/view/UserInteraction.java
@@ -1,8 +1,10 @@
 package edu.neumont.cox.dentistoffice.view;
 
+import lib.ConsoleIO;
+
 public class UserInteraction implements UserInteractionInterface{
 
 	public String adminPasswordChange() {
-		return null;
+		return ConsoleIO.promptForInput("Admin must change their password on first login.\nWhat do you want your new password to be?", false);
 	}
 }
