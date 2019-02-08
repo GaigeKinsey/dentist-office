dda5cc2540364455eaaddb38c14ee0513e7409e9
WistyDagel <chrisdazley@gmail.com>
2019-02-08 09:44:51 -0700

    Updated admin name

--- src/main/java/edu/neumont/cox/dentistoffice/view/UserInteraction.java
+++ src/main/java/edu/neumont/cox/dentistoffice/view/UserInteraction.java
@@ -12,7 +12,7 @@ public class UserInteraction implements UserInteractionInterface {
 
 	public String adminPasswordChange() {
 		return ConsoleIO.promptForInput(
-				"Admin must change their password on first login.\nWhat do you want your new password to be?", false);
+				"Administrator must change their password on first login.\nWhat do you want your new password to be?", false);
 	}
 
 	@Override
