642c4df4f9d106e998b58a99527bbac47e51de08
WistyDagel <chrisdazley@gmail.com>
2019-02-10 10:37:23 -0700

    stubbed out javadoc

--- src/main/java/edu/neumont/cox/dentistoffice/DentistDriver.java
+++ src/main/java/edu/neumont/cox/dentistoffice/DentistDriver.java
@@ -3,7 +3,14 @@ package edu.neumont.cox.dentistoffice;
 import edu.neumont.cox.dentistoffice.controller.DentistController;
 import edu.neumont.cox.dentistoffice.view.UserInteraction;
 
+/**
+ * @author Chris and Gaige
+ *
+ */
 public class DentistDriver {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		DentistController controller = new DentistController(new UserInteraction());
 		controller.run();
